8b8fbfdaf5917843cc7af61b22c4142ea40462fd
jos50275266 <jos50275266@gmail.com>
2023-05-12 13:28:08 +0900

    Display Genres

--- VideoGame/video-game/src/App.tsx
+++ VideoGame/video-game/src/App.tsx
@@ -1,31 +1,31 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
-    <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`, // 1024px
-        }}
-      >
-        <GridItem area="nav">
-          <NavBar />
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
         </GridItem>
-        <Show above="lg">
-          <GridItem area="aside">
-            <GenreList />
-          </GridItem>
-          <GridItem area="main">
-            <GameGrid />
-          </GridItem>
-        </Show>
-      </Grid>
-    </>
+      </Show>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
+    </Grid>
   );
 }
-
 export default App;
--- VideoGame/video-game/src/components/GameGrid.tsx
+++ VideoGame/video-game/src/components/GameGrid.tsx
@@ -6,30 +6,27 @@ import GameCardContainer from "./GameCardContainer";
 import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const { data: games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding="10px"
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
-            </GameCardContainer>
-          ))}
-        {games.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={3}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
