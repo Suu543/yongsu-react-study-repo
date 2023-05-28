a6e59e2c3ac9d8423ba063d76f0971a907ae9c1f
jos50275266 <jos50275266@gmail.com>
2023-05-28 18:53:22 +0900

    Setup Routing

--- part2-exercise-starter/src/App.tsx
+++ part2-exercise-starter/src/App.tsx
@@ -1,52 +1,7 @@
-import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import { useState } from "react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import useGameQueryStore from "./store";
-// undefined: the absence of a value
-// null: the intentional absence of a value
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const { gameQuery, setSearchText, setGenreId, setPlatformId, setSortOrder } =
-    useGameQueryStore();
-
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "250px 1fr",
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
-          <GenreList />
-        </GridItem>
-      </Show>
-      <GridItem area="main">
-        <Box paddingLeft={2}>
-          <GameHeading />
-          <Flex marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector />
-            </Box>
-            <SortSelector />
-          </Flex>
-        </Box>
-        <GameGrid />
-      </GridItem>
-    </Grid>
-  );
+  return <HomePage />;
 }
 
 export default App;
